169820c42018ae59c6d3fa8e7a40ea2ae2cc9bd4
pmoril-settings <53253888+pmoril-settings@users.noreply.github.com>
2019-09-10 14:51:19 +0200

    Update fresh-install.sh

--- fresh-install.sh
+++ fresh-install.sh
@@ -35,13 +35,13 @@ installKubectl() {
 }
 
 installMinikube() {
-	sudo apt install -y libvirt-clients libvirt-daemon-system qemu-kvm \
-  	  && sudo usermod -a -G libvirt $(whoami) \
-  	  && newgrp libvirt
-	curl -LO https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-kvm2 \
-	   && sudo install docker-machine-driver-kvm2 /usr/local/bin/
-	curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
-	  && sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+	sudo apt-get install -y libvirt-clients libvirt-daemon-system qemu-kvm
+  	sudo usermod -a -G libvirt $(whoami)
+  	newgrp libvirt
+	curl -LO https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-kvm2
+	sudo install docker-machine-driver-kvm2 /usr/local/bin/
+	curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+	sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
 	minikube config set vm-driver kvm2
 }
 
