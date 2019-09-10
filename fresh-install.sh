08f18e4e4b990c6963f2ce947c3e9c95dec6bc23
pmoril-settings <53253888+pmoril-settings@users.noreply.github.com>
2019-09-10 16:08:06 +0200

    Update fresh-install.sh

--- fresh-install.sh
+++ fresh-install.sh
@@ -26,6 +26,9 @@ installDocker() {
         sudo apt-get update
         sudo apt-get install -y docker-ce docker-ce-cli containerd.io
         sudo usermod -aG docker $USER
+	su - $USER
+	sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	sudo chmod +x /usr/local/bin/docker-compose
 }
 
 installKubectl() {
