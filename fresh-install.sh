4ced9d479b74fbe0ce5eed226a40214ddb134aa9
pmoril-settings <53253888+pmoril-settings@users.noreply.github.com>
2020-10-28 20:29:49 +0100

    Update fresh-install.sh
    update docker-compose version

--- fresh-install.sh
+++ fresh-install.sh
@@ -26,7 +26,7 @@ installDocker() {
         sudo usermod -aG docker $USER
 	# TODO: Discover why this line produces an exit
 	# su - $USER
-	sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 	sudo chmod +x /usr/local/bin/docker-compose
 }
 
