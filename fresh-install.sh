76d6ddd2c293ad304272a89f19451385640cfdc0
pmoril-settings <53253888+pmoril-settings@users.noreply.github.com>
2020-10-28 20:28:11 +0100

    Update fresh-install.sh
    add shell variable for release

--- fresh-install.sh
+++ fresh-install.sh
@@ -19,7 +19,7 @@ installDocker() {
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
         sudo add-apt-repository \
           "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-          disco \
+          $(lsb_release -cs) \
           stable"
         sudo apt-get update
         sudo apt-get install -y docker-ce docker-ce-cli containerd.io
